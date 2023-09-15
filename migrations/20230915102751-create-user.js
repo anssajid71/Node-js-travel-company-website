feb80eed8f3d1311f215d683f291aff0fad3ec72
Muhammad Anas Sajid <muhammadanassajid@MP15-MAnas-TSE-159.local>
2023-09-15 17:05:08 +0500

    add seed

--- migrations/20230915102751-create-user.js
+++ migrations/20230915102751-create-user.js
@@ -1,28 +1,82 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('user', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
+
+      Name: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      createdAt: {
+      password: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.STRING,
       },
-      updatedAt: {
+      retype_password: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.STRING,
+      },
+
+      user_type: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.ENUM,
+      },
+    });
+
+    await queryInterface.createTable('registercompanies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        refrences: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+
+      company_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      company_logo: {
+        type: Sequelize.STRING,
+      },
+      contact_person: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      contact_email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+
+      contact_phone: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+    await queryInterface.dropAllTables();
+  },
 };
